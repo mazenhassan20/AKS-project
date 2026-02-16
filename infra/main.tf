3cc288c58f4212565f9879e64a7ceeb39bfc15ec
mazenhassan20 <mazenelsayad99@gmail.com>
2026-02-16 05:08:37 +0200

    Refactor Azure backend configuration in main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -6,12 +6,11 @@ terraform {
     }
   }
 
-  # 👇👇 ده الجزء الجديد اللي هينقل الملف لأزور 👇👇
   backend "azurerm" {
-    resource_group_name  = "tfstate-rg"        # اسم الجروب الخاص بالـ State
-    storage_account_name = "mazentfstate9988"  # اسم الستورج اللي عملناه (تأكد إنه نفس الاسم اللي عملته)
-    container_name       = "tfstate"           # اسم الحاوية
-    key                  = "terraform.tfstate" # اسم الملف
+    resource_group_name  = "tfstate-rg"       
+    storage_account_name = "mazentfstate9988" 
+    container_name       = "tfstate"           
+    key                  = "terraform.tfstate" 
   }
 }
 
@@ -19,18 +18,15 @@ provider "azurerm" {
   features {}
 }
 
-# 1. استدعاء الـ Resource Group الموجودة مسبقاً
 data "azurerm_resource_group" "rg" {
   name = "my-devops-project-rg"
 }
 
-# 2. استدعاء الـ ACR الموجود مسبقاً (عشان نربطه بالـ AKS)
 data "azurerm_container_registry" "acr" {
-  name                = "mazenelsayad1234" # اسم الـ ACR بتاعك
+  name                = "mazenelsayad1234"
   resource_group_name = data.azurerm_resource_group.rg.name
 }
 
-# 3. إنشاء الـ AKS Cluster (السيرفر الرئيسي)
 resource "azurerm_kubernetes_cluster" "aks" {
   name                = "my-aks-cluster"
   location            = data.azurerm_resource_group.rg.location
@@ -39,8 +35,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1                # نود واحدة كفاية للتجربة والتوفير
-    vm_size    = "Standard_B2s_v2" # تأكد إن المقاس ده متاح في region النمسا
+    node_count = 1               
+    vm_size    = "Standard_B2s_v2" 
   }
 
   identity {
@@ -52,7 +48,6 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 }
 
-# 4. إعطاء صلاحية للـ AKS إنه يسحب صور من الـ ACR (AcrPull)
 resource "azurerm_role_assignment" "aks_acr_pull" {
   principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
   role_definition_name             = "AcrPull"
